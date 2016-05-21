e1a709444b250b2f5aef9d1e39ec4b3eaa128fd0
Carlos Sanz Garcia <studiocam6@yahoo.es>
2016-05-21 18:16:54 +0200

    Source structure /src

---
+++ src/app.js
@@ -0,0 +1,5 @@
+require('file?name=[name].[ext]!./index.html');
+require('file?name=css/[name].[ext]!../node_modules/todomvc-common/base.css');
+require('file?name=css/[name].[ext]!../node_modules/todomvc-app-css/index.css');
+
+console.log('main js file');
